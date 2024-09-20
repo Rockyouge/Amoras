3d889d01b8d48097f6466c121b2e51d4a6abe42b
Cass <cassixfyy6702@gmail.com>
2024-09-20 07:51:42 -0300

    criando models

--- amoras/livros/models.py
+++ amoras/livros/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.CharField(max_length=100)
+    publicado_em = models.DateField()
+
+    def __str__(self):
+        return f"{self.titulo} por {self.autor}"
+
--- amoras/livros/views.py
+++ amoras/livros/views.py
@@ -4,15 +4,70 @@ from django.utils.decorators import method_decorator
 from django.http  import HttpResponse 
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
+from .models import Livro
 
 
 @method_decorator(csrf_exempt, name='dispatch')
 class LivroViews(View):
     def get(self, request):
-        return HttpResponse("ola, amoras! Metodo get chamado com sucesso!")
+        livros = Livro.objects.all()
+        livros_list = [f"ID: {livro.id}, Título: {livro.titulo}, Autor: {livro.autor}, Publicado em: {livro.publicado_em}" for livro in livros]
+        response_content = "\n".join(livros_list)
+        return HttpResponse(response_content, content_type="text/plain")
+
+    
     def post(self, request):
-        return HttpResponse("ola, amoras! Metodo POST chamado com sucesso!")
+        try:
+            # Acessa dados de formulário
+            titulo = request.POST.get('titulo')
+            autor = request.POST.get('autor')
+            publicado_em = request.POST.get('publicado_em')
+
+            if not titulo or not autor or not publicado_em:
+                return HttpResponse('Todos os campos são necessários.', status=400)
+
+            livro = Livro.objects.create(
+                titulo=titulo,
+                autor=autor,
+                publicado_em=publicado_em
+            )
+            return HttpResponse(f'Livro criado com sucesso! ID: {livro.id}', status=201)
+        except Exception as e:
+            return HttpResponse(f'Erro inesperado: {str(e)}', status=500)
+        
     def put(self, request):
-        return HttpResponse("ola, amoras! Metodo PUT chamado com sucesso!")
+        try:
+            # Acessa dados de formulário
+            livro_id = request.POST.get('id')
+            if not livro_id:
+                return HttpResponse('ID do livro é necessário.', status=400)
+                
+            livro = Livro.objects.get(id=livro_id)
+            livro.titulo = request.POST.get('titulo', livro.titulo)
+            livro.autor = request.POST.get('autor', livro.autor)
+            livro.publicado_em = request.POST.get('publicado_em', livro.publicado_em)
+            livro.save()
+            
+            return HttpResponse('Livro atualizado com sucesso!')
+        except Livro.DoesNotExist:
+            return HttpResponse('Livro não encontrado', status=404)
+        except Exception as e:
+            return HttpResponse(f'Erro inesperado: {str(e)}', status=500)
+      
+
     def delete(self, request):
-        return HttpResponse("ola, amoras! Metodo DELETE chamado com sucesso!")
+        try:
+            # Acessa dados de formulário
+            livro_id = request.POST.get('id')
+            if not livro_id:
+                return HttpResponse('ID do livro é necessário.', status=400)
+
+            livro = Livro.objects.get(id=livro_id)
+            livro.delete()
+            
+            return HttpResponse('Livro deletado com sucesso!', status=204)
+        
+        except Livro.DoesNotExist:
+            return HttpResponse('Livro não encontrado', status=404)
+        except Exception as e:
+            return HttpResponse(f'Erro inesperado: {str(e)}', status=500)
